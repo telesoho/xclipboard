5d43172bcfeda107d58e3cbb10f0061d83ba15ea
telesoho <telesoho@gmail.com>
2023-04-28 18:17:04 +0800

    test: fix error

--- linux-save-clipbaord-png.sh
+++ linux-save-clipbaord-png.sh
@@ -5,10 +5,10 @@ command -v xclip >/dev/null 2>&1 || { echo >&1 "no xclip"; exit 1; }
 
 # write image in clipboard to file (see http://unix.stackexchange.com/questions/145131/copy-image-from-clipboard-to-file)
 if
-xclip -selection clipboard -target image/png -o >/dev/null 2>&1
+    xclip -selection clipboard -target image/png -o >/dev/null 2>&1
 then
-xclip -selection clipboard -target image/png -o >$1 2>/dev/null
-echo $1
+    xclip -selection clipboard -target image/png -o >$1 2>/dev/null
+    echo $1
 else
-echo "no image"
+    echo "no image"
 fi
